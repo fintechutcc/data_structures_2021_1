42ee06c92e0c91d265b532090ffbfddf585ccd69
fintechutcc <48086552+fintechutcc@users.noreply.github.com>
2021-08-30 11:54:13 +0700

    Updated LinkedList.

---
+++ Data Structures 2021/src/th/ac/utcc/eng/nc231/CarLinkedList.java
@@ -0,0 +1,50 @@
+package th.ac.utcc.eng.nc231;
+
+public class CarLinkedList {
+	private CarNode first;
+	
+	public CarLinkedList() {
+		first = null;
+	}
+	
+	public void insertFirst(CarNode node) {
+		node.next = first;
+		first = node;
+	}
+	
+	public void insertFirst(Car c) {
+		// สร้าง CarNode
+		CarNode node = new CarNode();
+		node.car = c;
+		
+		node.next = first;
+		first = node;
+	}
+	
+	public CarNode find(int id) {
+		CarNode current = first;
+		while(current.car.getId() != id) {
+			if(current.next == null)
+				return null;
+			else
+				current = current.next;
+		}
+		
+		return current;
+	}
+	
+	public CarNode delete(int id) {
+		CarNode current = first;
+		// TODO
+		
+		return current;
+	}
+	
+	public void displayList() {
+		CarNode current = first;
+		while(current != null) {
+			System.out.println(current.car);
+			current = current.next;
+		}
+	}
+}
